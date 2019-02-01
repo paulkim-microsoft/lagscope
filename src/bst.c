083cf99725dcffef5147909c8f3e76b3ca6f1d65
Paul Kim <v-paulki@microsoft.com>
2019-02-01 11:05:09 -0800

    hmm

--- src/bst.c
+++ src/bst.c
@@ -2,6 +2,7 @@
 
 struct Node *new_node(double lat)
 {
+    printf("Created New Node\n");
     struct Node *lat_node = (struct Node *)malloc(sizeof(struct Node));
     lat_node->lat = lat;
     lat_node->left = NULL;
@@ -12,24 +13,27 @@ struct Node *new_node(double lat)
 
 struct Node *insert(struct Node *node, double lat)
 {
-    printf("Insert function Check\n");
     if(node == NULL)
     {
+        printf("Insert function Check 1\n");
         return new_node(lat);
     }
 
     if(lat == node->lat)
     {
+        printf("Insert function Check 2\n");
         (node->count)++;
         return node;
     }
 
     if(lat < node->lat)
     {
+        printf("Insert function Check 3\n");
         node->left = insert(node->left, lat);
     }
     else
     {
+        printf("Insert function Check 4\n");
         node->right = insert(node->right, lat);
     }
 
